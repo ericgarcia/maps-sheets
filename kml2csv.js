d4b50416976e6041e6da337b7c296b7db6f38925
Eric Garcia <eric.garcia@gmail.com>
2015-09-09 15:00:40 -0300

    can parse listing with images

--- kml2csv.js
+++ kml2csv.js
@@ -7,9 +7,14 @@ var jsdom = require('jsdom').jsdom;
 var filenameKml = process.argv[2]; //i.e. Leads.kml
 var filenameTsv = filenameKml.split('.')[0]+'.tsv'
 
-var header = ['name','contact','email','website','country','ambassador','location'];
+// var header = ['name','contact','email','website','country','ambassador','location'];
+var header = ['name','description','location'];
 
 var rawKml      = fs.readFileSync(filenameKml, 'utf8');
+// Remove markup from urls and images
+rawKml = rawKml.replace(/\<\!\[CDATA\[(\+INFO, )?(\+ INFO\<br\>\<br\>)?(.*)\<br\>\<br\>gx_image_links\:.*]]>/g, "$3");
+rawKml = rawKml.replace(/\+ info.*? (\-)?(.*)/gi,"$2");
+
 var converted   = tj.kml(jsdom(rawKml));
 
 var properties  = _.pluck(converted.features,'properties');
@@ -23,5 +28,7 @@ _.forEach(properties, function (props,i) {
   })
   output += line.join('\t')+'\n';
 })
+
+console.log(output)
 
 fs.writeFileSync(filenameTsv, output);
